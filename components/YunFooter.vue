83dc17a844129d6b90449f20a858d9267499f71a
dreamerhe114514 <dreamerhe7747@hotmail.com>
2025-02-11 20:38:35 +0800

    first commit

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -1,3 +1,3 @@
 <template>
-    <a href="https://icp.gov.moe/?keyword=20250747" target="_blank">萌ICP备20250747号</a>
+    <a href="https://icp.gov.moe/?keyword=20259747" target="_blank">萌ICP备20259747号</a>
 </template>
